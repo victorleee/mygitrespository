679390565d94b11d32bd0592a9a228ad821b183e
victorleee <851187951@qq.com>
2015-12-09 20:40:08 +0800

    firt commit

---
+++ src/main/java/com/sohu/tv/train/App.java
@@ -0,0 +1,13 @@
+package com.sohu.tv.train;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
